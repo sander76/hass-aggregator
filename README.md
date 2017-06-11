a22a52b7b9542f82248f93acedb266553a0e3284
sanders76 <s.teunissen@gmail.com>
2017-06-11 08:26:41 +0200

    more aggregator functions.

---
+++ README.md
@@ -0,0 +1,5 @@
+# HASS aggregator
+
+An attempt to aggregate sensor data and have them written to the database.
+It allows for minimizing the amount of data sent to the database without completely skipping any logging of sensor data.
+
--- hass_aggregator.py
+++ hass_aggregator.py
@@ -1,12 +1,12 @@
 """yaml
-hass-aggregator
+hass-aggregator:
     - entity_id: <an id>
       aggregator:
         method: skip
         range: 10
     - entity_id: <an id>
       aggregator:
-        method: average
+        method: max
         range: 4
     - entity_id: <an id>
       aggregator:
@@ -14,6 +14,7 @@ hass-aggregator
         upper: <upper value to cap.>
         lower: <lower value to cap.>
 
+average
 max
 min
 cap
@@ -29,7 +30,7 @@ from homeassistant.core import callback
 from homeassistant.helpers.entity import async_generate_entity_id, Entity
 from homeassistant.helpers.entity_component import EntityComponent
 
-DOMAIN = 'hass-aggregator'
+DOMAIN = 'hass_aggregator'
 
 ENTITY_ID_FORMAT = DOMAIN + '.{}'
 
@@ -39,11 +40,13 @@ _LOGGER = logging.getLogger(__name__)
 @asyncio.coroutine
 def async_setup(hass, config):
     component = EntityComponent(_LOGGER, DOMAIN, hass)
-    entities = []
+
+    entities = [AggregatedEntity(hass, aggregator)
+                for aggregator in config[DOMAIN]]
 
     @callback
     def aggregate(_event):
-        new_state = _event.date.get('new_state')
+        new_state = _event.data.get('new_state')
         if new_state:
             for entity in entities:
                 entity.aggregate(new_state)
@@ -52,33 +55,81 @@ def async_setup(hass, config):
         EVENT_STATE_CHANGED,
         aggregate
     )
+    yield from component.async_add_entities(entities)
     return True
 
 
 class BaseAggrFunction():
-    def __init__(self, name):
+    def __init__(self, name, range=None):
+        self.state = None
         self._name = name
+        self._attributes = {"method": name}
+        if range:
+            self._range = range
+            self._attributes['range'] = range
+            self._current_range = 0
 
     @property
     def name(self):
         return self._name
 
-    def aggregate(self,state):
+    def _check_range(self):
+        if self._current_range == self._range:
+            self._current_range = 0
+            return True
+        else:
+            self._current_range += 1
+            return False
+
+    def aggregate(self, state):
         raise NotImplemented
 
 
 class AggrSkip(BaseAggrFunction):
     def __init__(self, aggregator):
-        BaseAggrFunction.__init__(self, 'skip')
-        self.range = aggregator.get('range')
-        self.current_range = 0
+        BaseAggrFunction.__init__(self, 'skip',
+                                  range=aggregator.get('range'))
 
     def aggregate(self, state):
-        if self.current_range == self.range:
-            self.current_range = 0
+        if self._check_range():
             return state
         else:
-            self.current_range += 1
+            return None
+
+
+class AggrMax(BaseAggrFunction):
+    def __init__(self, aggregator):
+        BaseAggrFunction.__init__(self, 'max', range=aggregator.get('range'))
+
+    def _check_max(self, new_val):
+        if self.state is None:
+            self.state = new_val
+        else:
+            self.state = max(self.state, new_val)
+
+    def aggregate(self, state):
+        self._check_max(state)
+        if self._check_range():
+            return self.state
+        else:
+            return None
+
+
+class AggrMin(BaseAggrFunction):
+    def __init__(self, aggregator):
+        BaseAggrFunction.__init__(self, 'min', range=aggregator.get('range'))
+
+    def _check_min(self, new_val):
+        if self.state is None:
+            self.state = new_val
+        else:
+            self.state = min(self.state, new_val)
+
+    def aggregate(self, state):
+        self._check_min(state)
+        if self._check_range():
+            return self.state
+        else:
             return None
 
 
@@ -93,8 +144,8 @@ class AggregatedEntity(Entity):
         self._state = STATE_UNKNOWN
         self.aggregator = get_aggregator(aggregator.get('aggregator'))
 
-        self.id_to_aggregate = aggregator.get('id_to_aggregate')
-        self.aggregated_id = '{}.{}'.format(self.id_to_aggregate,
+        self.id_to_aggregate = (aggregator.get('entity_id')).replace('.', ' ')
+        self.aggregated_id = '{} {}'.format(self.id_to_aggregate,
                                             self.aggregator.name)
         self.entity_id = async_generate_entity_id(
             ENTITY_ID_FORMAT,
@@ -106,6 +157,10 @@ class AggregatedEntity(Entity):
             _state = self.aggregator.aggregate(new_state.state)
             if _state:
                 self._state = _state
+
+    @property
+    def state_attributes(self):
+        return self.aggregator._attributes
 
     @property
     def state(self):
